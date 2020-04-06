ac0395993c87a3b66a1197c1cd4f1d56eb392b37
Stefan Zhelyazkov <stefan.zhelyazkov@gmail.com>
2020-04-06 15:26:57 +0300

    Use ExoPlayer to play videos

--- app/src/main/java/com/stefanzh/btvseriali/DisplayClipActivity.kt
+++ app/src/main/java/com/stefanzh/btvseriali/DisplayClipActivity.kt
@@ -1,12 +1,16 @@
 package com.stefanzh.btvseriali
 
-import android.content.res.Configuration
+import android.annotation.SuppressLint
 import android.net.Uri
 import android.os.Bundle
 import android.view.MenuItem
-import android.widget.MediaController
-import android.widget.VideoView
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.ui.PlayerView
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.util.Util
 import io.ktor.client.request.get
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -17,12 +21,19 @@ class DisplayClipActivity : AppCompatActivity() {
 
     companion object {
         const val EXTRA_EPIZOD = "com.stefanzh.btvseriali.EPIZOD"
+
+        // RegEx to capture the video clip source location on the bTV website
+        val CLIP_REGEX = Regex("(//vid\\.btv\\.bg[\\w\\d/-]+\\.mp4)", RegexOption.MULTILINE)
     }
 
-    private lateinit var videoView: VideoView
+    private lateinit var tvShowEpisodeLink: String
+    private var playerView: PlayerView? = null
+    private var player: SimpleExoPlayer? = null
 
-    // RegEx to capture the video clip source location on the bTV website
-    private val clipRegEx = Regex("(//vid\\.btv\\.bg[\\w\\d/-]+\\.mp4)", RegexOption.MULTILINE)
+    // Player state params
+    private var playWhenReady = true
+    private var currentWindow = 0
+    private var playbackPosition: Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,28 +41,93 @@ class DisplayClipActivity : AppCompatActivity() {
         setContentView(R.layout.activity_loading)
 
         // Get the link to the TV show episode
-        val tvShowEpisodeLink = intent.getStringExtra(EXTRA_EPIZOD)!!
+        tvShowEpisodeLink = intent.getStringExtra(EXTRA_EPIZOD)!!
+    }
 
-        CoroutineScope(Dispatchers.Main).launch {
-            val videoClipUrl = getEpisodeClipUrl(tvShowEpisodeLink)
+    /**
+     * Prepares the Player to play the Media for this Activity.
+     */
+    private fun initializePlayer() = CoroutineScope(Dispatchers.Main).launch {
+        val videoClipUrl = getEpisodeClipUrl(tvShowEpisodeLink)
 
-            // if TV show episode is retrieved successfully, set the view to the clip display activity
-            setContentView(R.layout.activity_display_clip)
+        // if TV show episode is retrieved successfully, set the view to the clip display activity
+        setContentView(R.layout.activity_display_clip)
+        playerView = findViewById(R.id.episode_clip)
 
-            // set the layout and play the clip
-            videoView = findViewById(R.id.episode_clip)
-            val uri = Uri.parse(videoClipUrl)
+        // set the player
+        player = SimpleExoPlayer.Builder(this@DisplayClipActivity).build()
+        playerView?.player = player
 
-            // create media controller
-            val mediaController = MediaController(this@DisplayClipActivity)
-            mediaController.setAnchorView(videoView)
-            mediaController.setMediaPlayer(videoView)
+        // build the MediaSource from the URI
+        val uri = Uri.parse(videoClipUrl)
+        val dataSourceFactory = DefaultDataSourceFactory(this@DisplayClipActivity, "exoplayer-agent")
+        val mediaSource = ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(uri)
 
-            // video view
-            videoView.setVideoURI(uri)
-            videoView.setMediaController(mediaController)
-            videoView.start()
+        // use stored state (if any) to resume (or start) playback
+        player?.playWhenReady = playWhenReady
+        player?.seekTo(currentWindow, playbackPosition)
+        player?.prepare(mediaSource, false, false)
+    }
+
+    /**
+     * Starting with API level 24 Android supports multiple windows. As our app can be visible but
+     * not active in split window mode, we need to initialize the player in onStart. Before API level
+     * 24 we wait as long as possible until we grab resources, so we wait until onResume before
+     * initializing the player.
+     */
+    override fun onStart() {
+        super.onStart()
+        if (Util.SDK_INT >= 24) {
+            initializePlayer()
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        hideSystemUi()
+        if (Util.SDK_INT < 24 || player == null) {
+            initializePlayer()
+        }
+    }
+
+    /**
+     * Before API Level 24 there is no guarantee of onStop being called. So we have to release the
+     * player as early as possible in onPause. Starting with API Level 24 (which brought multi and
+     * split window mode) onStop is guaranteed to be called. In the paused state our activity is still
+     * visible so we wait to release the player until onStop.
+     */
+    override fun onPause() {
+        super.onPause()
+        if (Util.SDK_INT < 24) {
+            releasePlayer()
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (Util.SDK_INT >= 24) {
+            releasePlayer()
+        }
+    }
+
+    @SuppressLint("InlinedApi")
+    private fun hideSystemUi() {
+        playerView?.systemUiVisibility = (View.SYSTEM_UI_FLAG_LOW_PROFILE
+            or View.SYSTEM_UI_FLAG_FULLSCREEN
+            or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+            or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
+    }
+
+    private fun releasePlayer() {
+        player?.let {
+            playWhenReady = it.playWhenReady
+            playbackPosition = it.currentPosition
+            currentWindow = it.currentWindowIndex
+            it.release()
+        }
+        player = null
     }
 
     // When back button is pressed, close this activity, which will go back to previous screen
@@ -64,20 +140,12 @@ class DisplayClipActivity : AppCompatActivity() {
     }
 
     /**
-     * When the orientation changes, we need to resize the VideoView
-     */
-    override fun onConfigurationChanged(newConfig: Configuration) {
-        super.onConfigurationChanged(newConfig)
-        videoView.holder.setSizeFromLayout()
-    }
-
-    /**
      * Extracts the video clip link of the TV show.
      * The HTTP request for the video clip link is executed on the IO-optimized thread.
      */
     private suspend fun getEpisodeClipUrl(link: String): String = withContext(Dispatchers.IO) {
         // parses the HTML and extracts the links to the TV shows
         val response = MainActivity.client.get<String>(link)
-        clipRegEx.find(response)!!.value.prefixURL()
+        CLIP_REGEX.find(response)!!.value.prefixURL()
     }
 }
