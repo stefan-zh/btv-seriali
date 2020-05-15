910736cf939a2ee3d9e77fc8ecd99d96780fbb1d
Stefan Zhelyazkov <stefan.zhelyazkov@gmail.com>
2020-05-15 14:46:51 +0300

    С любов за маминка

--- app/src/main/java/com/stefanzh/beetvplus/activity/AboutActivity.kt
+++ app/src/main/java/com/stefanzh/beetvplus/activity/AboutActivity.kt
@@ -1,7 +1,7 @@
 package com.stefanzh.beetvplus.activity
 
 import android.os.Bundle
-import android.widget.LinearLayout
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.stefanzh.beetvplus.R
 
@@ -10,10 +10,21 @@ class AboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+        // show the back button
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        title = getString(R.string.action_about)
+    }
 
-        // on click on the view, close the Activity
-        findViewById<LinearLayout>(R.id.about_layout).setOnClickListener {
+    /**
+     * In the other activities we extend from [CastingActivity], which already supports
+     * the functionality below, but here we are using a simple [AppCompatActivity]
+     * so we need to add the back button functionality.
+     */
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
             finish()
+            return true
         }
+        return super.onOptionsItemSelected(item)
     }
 }
