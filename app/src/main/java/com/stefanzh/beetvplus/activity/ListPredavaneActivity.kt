5307f0bd850ee1ee88423f01de7ec22bf4af46b6
Stefan Zhelyazkov <stefan.zhelyazkov@gmail.com>
2021-02-06 21:10:56 +0200

    Fix for tv shows where source DOM is incomplete and version 2.5

--- app/src/main/java/com/stefanzh/beetvplus/activity/ListPredavaneActivity.kt
+++ app/src/main/java/com/stefanzh/beetvplus/activity/ListPredavaneActivity.kt
@@ -13,6 +13,7 @@ import com.stefanzh.beetvplus.adapter.TvShowAdapter
 import io.ktor.client.request.get
 import kotlinx.coroutines.*
 import org.jsoup.Jsoup
+import org.jsoup.nodes.Element
 import java.net.URL
 
 
@@ -51,6 +52,30 @@ class ListPredavaneActivity : CastingActivity() {
     }
 
     /**
+     * Get the data for an individual TV show. Sometimes the DOM for the TV show may be
+     * corrupted or incomplete so we'll return null for such shows (and will later skip them).
+     */
+    private suspend fun getTvShow(el: Element): SerialLink? = withContext(Dispatchers.IO) {
+        val href = el.attr("href")
+        val link = "https://btvplus.bg$href"
+        val imgSrc = el.select("img").attr("src").prefixURL()
+        try {
+            val image = URL(imgSrc).toImageBitmap()
+            // extract TV show name
+            val showPage = client.get<String>(link)
+            val title = Jsoup.parse(showPage).select("div.title > h2").text().ifBlank {
+                // we'll consider a show without a title to be improper
+                return@withContext null
+            }
+            SerialLink(link, imgSrc, image, title)
+        } catch (e: Exception) {
+            // it's possible that there are errors on the DOM
+            // so we'll skip such shows
+            null
+        }
+    }
+
+    /**
      * Extracts the links to the TV shows.
      * The HTTP request for shows is executed on the IO-optimized thread.
      */
@@ -62,19 +87,9 @@ class ListPredavaneActivity : CastingActivity() {
         val asyncTasks = categories.flatMap { cat ->
             val anchors = cat.select("div.image > a")
             anchors.map {
-                async {
-                    val href = it.attr("href")
-                    val link = "https://btvplus.bg$href"
-                    val imgSrc = it.select("img").attr("src").prefixURL()
-                    val image = URL(imgSrc).toImageBitmap()
-
-                    // extract TV show name
-                    val showPage = client.get<String>(link)
-                    val title = Jsoup.parse(showPage).select("div.title > h2").text()
-                    SerialLink(link, imgSrc, image, title)
-                }
+                async { getTvShow(it) }
             }
         }
-        asyncTasks.awaitAll()
+        asyncTasks.awaitAll().filterNotNull()
     }
 }
